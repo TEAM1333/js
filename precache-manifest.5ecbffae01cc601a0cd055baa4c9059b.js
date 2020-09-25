d56b402590f87cc8bc468abd67f74cf038dacce6
SAT-engines Devoloper <pelserwaldo@gmail.com>
2020-09-25 14:50:37 +0200

    first commit

---
+++ precache-manifest.5ecbffae01cc601a0cd055baa4c9059b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ea1eb55208e8a914c124f6e70627de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a78ec95d4efdbf254b3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "16dc095645b2efdbf054",
+    "url": "/static/js/2.6046a761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6046a761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a78ec95d4efdbf254b3",
+    "url": "/static/js/main.44ee2357.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
